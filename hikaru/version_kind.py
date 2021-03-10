b51d29f6bf0a92bd5c9ea63a5b24f75cf002a75e
tom <tcarroll@incisivetech.co.uk>
2021-03-10 13:25:33 +0000

    Fixed the adding of a 'version_' class attribute to only those classes that appear to be able to be top-level document containers. Added all of the copyright headers to standard and generated code. Made some changes so the build isn't as fragile as before.

--- hikaru/version_kind.py
+++ hikaru/version_kind.py
@@ -1,3 +1,23 @@
+#
+# Copyright (c) 2021 Incisive Technology Ltd
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 import importlib
 from hikaru.meta import HikaruDocumentBase
 try:
@@ -16,4 +36,3 @@ for version in versions:
         if (type(o) == type and issubclass(o, HikaruDocumentBase) and
                 o is not HikaruDocumentBase):
             version_kind_map[(version, o.__name__)] = o
-
